a03e441c78ca6c17fd23f7c847a4cf29cba1b444
Halil Bahar <halil.s.bahar@gmail.com>
2019-12-14 16:05:31 +0100

    Copy the codec instead if encoding the h264 to h264

--- ffmpeg/streamer.sh
+++ ffmpeg/streamer.sh
@@ -2,6 +2,6 @@
 IFS=';'
 while read -r url key || [ -n "$url" ]
 do
-    ffmpeg -re -stream_loop -1 -i $url -vcodec libx264 -f flv rtmp://srs/file/$key &
+    ffmpeg -re -stream_loop -1 -i $url -codec copy -f flv rtmp://srs/file/$key &
 done < /app/urls.txt
 wait
