6d5c4ff591e915c2e713bdfdb3c8b2eaf372636b
Halil Bahar <halil.s.bahar@gmail.com>
2019-12-17 20:13:41 +0100

    Change streaming from 'ffmpeg stream_loop' to playing a infinite playlist while every 15 minutes the files in the playlist are replaced with new ones

--- ffmpeg/streamer.sh
+++ ffmpeg/streamer.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+
+download_file() {
+    curl -s "$1" -o "$2_temp.mp4"
+    mv -f "$2_temp.mp4" "$2.mp4"
+}
+
 IFS=';'
 while read -r url key || [ -n "$url" ]
 do
-    ffmpeg -re -stream_loop -1 -i $url -codec copy -f flv rtmp://srs/file/$key &
-done < /app/urls.txt
-wait
+    download_file "$url" "$key"
+    urls+=("$url")
+    keys+=("$key")
+    printf "ffconcat version 1.0\nfile '%s.mp4'\nfile '%s.txt'" "$key" "$key" > "$key.txt"
+    ffmpeg -nostdin -re -f concat -i "$key.txt" -codec copy -f flv "rtmp://srs/file/$key" &
+done < urls.txt
+
+while true
+do
+    sleep 900
+    for i in "${!urls[@]}"
+    do
+        download_file "${urls[$i]}" "${keys[$i]}"
+    done
+done
